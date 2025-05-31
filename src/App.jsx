6e727d1ab363b87dfc7a0e97eef41bdd75554ed7
mekabu <sleep.seep.sheep@gmail.com>
2025-05-31 10:37:39 +0900

    misskey.ioのURLを修正

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         { url: 'https://github.com/00b7ce', title: '00b7ce', icon: 'github-mark-white.svg' },
         { url: 'https://x.com/mkb_rev2', title: '@mkb_rev2', icon: 'X_logo.svg' },
         { url: 'https://bsky.app/profile/bsky.mekabu.io', title: '@bsky.mekabu.io', icon: 'bluesky_media_kit_logo.svg' },
-        { url: 'https://bsky.app/profile/bsky.mekabu.io', title: '@mkb@misskey.io', icon: 'misskey.svg' },
+        { url: 'https://misskey.io/@mkb/', title: '@mkb@misskey.io', icon: 'misskey.svg' },
         // 他のリンクを追加
     ];
 
