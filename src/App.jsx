9f1205ffd7130e0361093280044424889088fcd2
mekabu <sleep.seep.sheep@gmail.com>
2025-06-03 07:20:46 +0900

    新しいブログを追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
     const links = [
-        { url: 'https://mkb-toolbox.hateblo.jp', title: 'Blog', icon: 'hatenablog-logo.svg' },
+        { url: 'https://blog.mekabu.io', title: 'Blog', icon: 'blog-icon.svg' },
+        { url: 'https://mkb-toolbox.hateblo.jp', title: 'Blog(old)', icon: 'hatenablog-logo.svg' },
         { url: 'https://github.com/00b7ce', title: '00b7ce', icon: 'github-mark-white.svg' },
         { url: 'https://x.com/mkb_rev2', title: '@mkb_rev2', icon: 'X_logo.svg' },
         { url: 'https://bsky.app/profile/bsky.mekabu.io', title: '@bsky.mekabu.io', icon: 'bluesky_media_kit_logo.svg' },
